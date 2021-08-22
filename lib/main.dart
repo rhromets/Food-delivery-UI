76aef16679808df4f549ffc7c8a37c6fe4bf5aa0
roman <rhromets@gmail.com>
2021-08-22 14:54:15 +0300

    Food Delivery Init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Food Delivery UI',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[50],
+        primaryColor: Colors.deepOrangeAccent,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
